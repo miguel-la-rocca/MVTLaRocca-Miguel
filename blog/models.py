9089d1e10d7edc95a4d46985e7d35421c1760466
miguel-la-rocca <ing.miguellarocca@gmail.com>
2022-10-25 20:10:47 -0300

    agregamos admin y config blog

--- blog/models.py
+++ blog/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 class Configuracion(models.Model):
     nombre_blog = models.CharField(max_length=14)
-    
+    contruido_por = models.CharField(max_length=30)
+    titulo_principal = models.CharField(max_length=40)
+    subtitulo_principal = models.CharField(max_length=40)
