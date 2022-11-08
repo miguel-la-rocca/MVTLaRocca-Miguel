60570b8e7ae564f58addb371028622529708be42
miguel-la-rocca <ing.miguellarocca@gmail.com>
2022-11-08 20:37:53 -0300

    paginacion

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from blog.models import Configuracion
+
 from blog.models import Post
 
-admin.site.register(Configuracion)
+
 
 admin.site.register(Post)
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
-class Configuracion(models.Model):
-    nombre_blog = models.CharField(max_length=14)
-    contruido_por = models.CharField(max_length=30)
-    titulo_principal = models.CharField(max_length=40)
-    subtitulo_principal = models.CharField(max_length=40)
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
